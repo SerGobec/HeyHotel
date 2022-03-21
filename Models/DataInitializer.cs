685c3b7d1f9586000876271a5b1cd39bd43c05a4
SerGobec <48455848+SerGobec@users.noreply.github.com>
2022-03-21 19:46:49 +0200

    Role initializer has been created

--- Models/DataInitializer.cs
+++ Models/DataInitializer.cs
@@ -8,9 +8,35 @@ namespace HeyHotel.Models
 {
     public class DataInitializer
     {
-        public static void RoleInitialize(UserManager<User> userManager, RoleManager<IdentityRole> roleManager)
+        public static async Task RoleInitialize(UserManager<User> userManager, RoleManager<IdentityRole> roleManager)
         {
-            string adminGmail
+            // Later I will delete it, and hide it more secure :)
+            string adminGmail = "Sergobec@gmail.com";
+            string adminPassword = "Qwert_123";
+
+            if( await roleManager.FindByNameAsync("user") == null)
+            {
+                await roleManager.CreateAsync(new IdentityRole("user"));
+            }
+            if (await roleManager.FindByNameAsync("manager") == null)
+            {
+                await roleManager.CreateAsync(new IdentityRole("manager"));
+            }
+            if (await roleManager.FindByNameAsync("admin") == null)
+            {
+                await roleManager.CreateAsync(new IdentityRole("admin"));
+            }
+
+            if( await userManager.FindByNameAsync(adminGmail) == null )
+            {
+                User user = new User { Email = adminGmail, UserName = adminGmail };
+                IdentityResult result = await userManager.CreateAsync(user, adminPassword);
+                if (result.Succeeded)
+                {
+                    await userManager.AddToRoleAsync(user, "admin");
+                }
+
+            }
         }
     }
 }
