79384bb02f291dfefab7bef764614e98754c8a59
SerGobec <48455848+SerGobec@users.noreply.github.com>
2022-03-21 18:52:02 +0200

    Git ignored files deleted. Initial Data class added

---
+++ Models/DataInitializer.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HeyHotel.Models
+{
+    public class DataInitializer
+    {
+        public static void RoleInitialize(UserManager<User> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            string adminGmail
+        }
+    }
+}
