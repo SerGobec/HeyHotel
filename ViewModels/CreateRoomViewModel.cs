718109e613d5162da78359fb2050c4988c426dc7
SerGobec <48455848+SerGobec@users.noreply.github.com>
2022-03-26 19:00:35 +0200

    Create hotel and Create room functionality added

---
+++ ViewModels/CreateRoomViewModel.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HeyHotel.ViewModels
+{
+    public class CreateRoomViewModel
+    {
+        public List<SelectListItem> hotelsLocations { get; set; }
+        public int HotelId { get; set; }
+        public decimal Price { get; set; }
+        public int NumberOfRooms { get; set; }
+        public int Floor { get; set; }
+
+        public CreateRoomViewModel()
+        {
+            hotelsLocations = new List<SelectListItem>();
+        }
+    }
+}
